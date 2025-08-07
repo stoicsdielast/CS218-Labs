88fbc8f6fe9668b8a6495bc99e8da4a7ab06c4e3
Parvish Mohan <stoicsdielast@gmail.com>
2025-08-07 22:05:41 +1200

    Add Lab1 and Lab2 as regular folders

---
+++ Lab1_CS218/src/Main.kt
@@ -0,0 +1,47 @@
+fun main() {
+    println("Bula");
+    //Task 1&2
+    val name = "Parvish Mohan";
+    val currentCourse = "Software Engineering";
+    val courseName = "CS218: Mobile Middleware";
+    val mode = "Blended";
+    println("Hello my name is $name and i am doing bachelor of $currentCourse");
+    println("I am doing $mode mode")
+    println(courseName)
+
+
+    //Task 2
+    //Use of var when the variable keeps on changing
+    var currentScore = 0;
+    println(currentScore);
+    currentScore = 100;
+    println(currentScore);
+    //Task 3
+    println("Monday");
+    println("Tuesday");
+    println("Thursday");
+    println("Wednesday");
+    println("Friday");
+
+    println("Tomorrow is rainy")
+    println("Cloudy")
+    println("Partly Cloudy")
+    println("Windy")
+
+    val weatherForecast = "Sunny or Rainy"
+    println("How's the weather today? $weatherForecast")
+
+    //Task 6
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = baseSalary + bonusAmount
+    println("Congratulations for your bonus! You will receive a total of $  $totalSalary (additional bonus).")
+
+
+
+
+
+
+
+
+}
---
+++ Lab1_CS218/src/variables.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var num1 = 10
+    var num2 = 5
+    var num3 = 30;
+    var sum = num1 + num2
+    println(sum)
+    println("The sum of $num1 and $num2 is $sum.")
+
+    //swap two variables
+
+
+
+}
